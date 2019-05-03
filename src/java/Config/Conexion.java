914acc903626323ee80c764f79e70f776171bf24
Anomium <melvin.martinez.97@gmail.com>
2019-05-03 01:57:49 -0500

    Acomodando cadena de conexion

--- src/java/Config/Conexion.java
+++ src/java/Config/Conexion.java
@@ -9,7 +9,8 @@ public class Conexion {
     public Conexion() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/basedatos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "123456");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/basedatos", "root", "123456");
+            //con = DriverManager.getConnection("jdbc:mysql://localhost:3306/basedatos?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "123456");
         } catch (Exception e) {
             System.err.println("Conexion Error " + e);
         }
